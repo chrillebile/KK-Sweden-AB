bc1b93aea26b9f90bcb12aa726da61ff4f26ef01
chrillebile <chrillebile@gmail.com>
2018-03-22 13:53:02 +0100

    Added getPalletById

--- server/src/main/java/server/Controllers/PalletController.java
+++ server/src/main/java/server/Controllers/PalletController.java
@@ -6,20 +6,27 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import server.Models.Pallet;
 
 @RestController
+@RequestMapping(value = "/pallets")
 public class PalletController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/pallets")
+    @RequestMapping()
     public ResponseEntity<Pallet> greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new ResponseEntity<>(new Pallet(counter.incrementAndGet(),
                 0, new Date(), false, String.format(template, name) + "nej", new Timestamp(1521660285)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}")
+    public ResponseEntity<Pallet> getPalletById(@PathVariable("id") String id){
+        return null;
     }
 }
