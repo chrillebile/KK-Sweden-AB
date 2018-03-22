1d2b271ded23aee3f352a659710c27312c75662e
Ennio Mara <enniomara@users.noreply.github.com>
2018-03-22 11:19:34 +0100

    Decouples the controller from the model.
    The controller now returns a resource, which is based on a model.

--- server/src/main/java/server/Controllers/PalletController.java
+++ server/src/main/java/server/Controllers/PalletController.java
@@ -3,6 +3,9 @@ package server.Controllers;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,8 @@ public class PalletController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/pallets")
-    public Pallet greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Pallet(counter.incrementAndGet(),
-                            0, new Date(), false, String.format(template, name), new Timestamp(1521660285));
+    public ResponseEntity<Pallet> greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new ResponseEntity<>(new Pallet(counter.incrementAndGet(),
+                0, new Date(), false, String.format(template, name) + "nej", new Timestamp(1521660285)), HttpStatus.OK);
     }
 }
---
+++ server/src/main/java/server/Resources/PalletResource.java
@@ -0,0 +1,51 @@
+package server.Resources;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
+import server.Models.Pallet;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class PalletResource extends ResourceSupport {
+    private final long id;
+    private final int amount;
+    private final Date productionDate;
+    private final boolean isBlocked;
+    private final String location;
+    private final Timestamp deliveryTime;
+
+    public PalletResource(Pallet pallet) {
+        this.id = pallet.getId();
+        this.amount = pallet.getAmount();
+        this.productionDate = pallet.getProductionDate();
+        this.isBlocked = pallet.isBlocked();
+        this.location = pallet.getLocation();
+        this.deliveryTime = pallet.getDeliveryTime();
+    }
+
+    @JsonProperty("id")
+    public long getResourceId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Date getProductionDate() {
+        return productionDate;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Timestamp getDeliveryTime() {
+        return deliveryTime;
+    }
+}
